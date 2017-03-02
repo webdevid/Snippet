24891194bc5f8e16dff131fa4b65288f93b81d82
dodi hidayatullah <dodihidayatullah@gmail.com>
2017-03-02 15:24:12 +0700

    Create run-javascript-only-after-entire-page-has-loaded.js

---
+++ run-javascript-only-after-entire-page-has-loaded.js
@@ -0,0 +1,8 @@
+/*
+* Run JavaScript Only After Entire Page Has Loaded
+* https://css-tricks.com/snippets/jquery/run-javascript-only-after-entire-page-has-loaded/
+*/
+
+jQuery(window).bind("load", function() {
+   // code here
+});
